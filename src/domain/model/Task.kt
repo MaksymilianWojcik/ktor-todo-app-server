99315f75ab98fe9f7d74f7fd9b1ad08d08312abd
Maksymilian Wojcik <maksymilian.wojcik@intive.com>
2021-01-31 18:40:40 +0100

    Fixed task creation with necessary id and ownerid param

--- src/domain/model/Task.kt
+++ src/domain/model/Task.kt
@@ -2,12 +2,11 @@ package com.mwcode.server.domain.model
 
 import kotlinx.serialization.Serializable
 
-// TODO: Fix required id and ownerId in the post request for creating task
 @Serializable
 data class Task(
-    val id: Int,
+    val id: Int = 0,
     val name: String,
     val description: String,
     val isActive: Boolean,
-    val ownerId: Int = 0 // TODO: Temporary 0
+    val ownerId: Int = 0
 )
